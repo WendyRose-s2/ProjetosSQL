d2e4f1096e1e709ce0c2c80feab7cc3538795f2d
Wendy Rodrigues <151972765+WendyRose-s2@users.noreply.github.com>
2024-01-10 20:06:05 -0300

    Projeto GameOnline

--- Projeto RH.sql
+++ Projeto RH.sql
@@ -1,29 +1,56 @@
+CREATE DATABASE db_generation_gameonline;
 
-USE db_empresa;
+USE db_generation_gameonline;
 
-CREATE TABLE IF NOT EXISTS tb_rh (
-    id BIGINT AUTO_INCREMENT,
-    nome VARCHAR(255) NOT NULL,
-    cargo VARCHAR(50) NOT NULL,
-    salario DECIMAL(10,2) NOT NULL,
-    departamento VARCHAR(50),
-    data_admissao DATE,
-    PRIMARY KEY (id)
-    );
+CREATE TABLE tb_classes (
+id BIGINT auto_increment,
+nameclass VARCHAR(255) NOT NULL,
+tipo VARCHAR(50) NOT NULL,
+PRIMARY KEY (id)
+);
 
+INSERT INTO tb_classes (nameclass,tipo)
+VALUES ("Guerreiro","tanque"),
+("Mago","Mágico"),
+("Atirador","Range"),
+("Noviço","Suporte"),
+("Assassino","Melee");
 
-INSERT INTO colaboradores (nome, cargo, salario, departamento, data_admissao) VALUES
-('João Silva', 'Analista de RH', 2500.00, 'Recursos Humanos', '2021-01-15'),
-('Maria Oliveira', 'Programador', 1800.00, 'TI', '2020-05-20'),
-('Carlos Santos', 'Gerente de Vendas', 3000.00, 'Vendas', '2019-09-10'),
-('Ana Souza', 'Analista Financeiro', 2200.00, 'Financeiro', '2020-12-01'),
-('Pedro Pereira', 'Assistente Administrativo', 1900.00, 'Administração', '2021-03-25');
+CREATE TABLE tb_personagens (
+    personagem_id INT PRIMARY KEY auto_increment,
+    nome VARCHAR(50) NOT NULL,
+    nivel INT NOT NULL,
+    poder_ataque INT NOT NULL,
+    poder_defesa INT NOT NULL,
+    id BIGINT,
+    FOREIGN KEY (id) REFERENCES tb_classes(id)
+);
 
+INSERT INTO tb_personagens (nome,nivel,poder_ataque,poder_defesa,id)
+VALUES ("Caitlyn",26,260,64,3),
+("Leona",21,58,232,1),
+("Ahri",28,283,43,2),
+("Soraka",24,40,101,4),
+("Kalista",27,271,32,1),
+("Katarina",24,310,91,5),
+("Akali",19,131,63,5),
+("Poppy",23,41,309,1);
 
-SELECT * FROM colaboradores WHERE salario > 2000;
+SELECT * FROM tb_personagens WHERE poder_ataque > 200;
 
+SELECT * FROM tb_personagens WHERE poder_defesa BETWEEN 100 AND 200;
 
-SELECT * FROM colaboradores WHERE salario < 2000;
+SELECT * FROM tb_personagens WHERE nome LIKE '%C%';
 
+SELECT * FROM tb_personagens
+INNER JOIN tb_classes ON tb_personagens.id = tb_classes.id;
 
-UPDATE colaboradores SET salario = 2800.00 WHERE nome = 'João Silva';
+SELECT * FROM tb_personagens
+INNER JOIN tb_classes ON tb_personagens.id = tb_classes.id
+WHERE tb_classes.nameclass = 'Atirador';
+
+UPDATE tb_personagens
+SET id = 3
+WHERE nome = "Kalista";
+
+SET SQL_SAFE_UPDATES = 0;
